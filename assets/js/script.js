e7f3e6a6a5e67d2c342842cd680753d61d95c8e2
Tutor <tutor.michael78@gmail.com>
2021-07-13 20:28:22 -0500

    added the initial array of questions to be asked for the quiz

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+// variable containing the number of questions in the quiz
+var quizLength = 2;
+
+// array containing all of the questions and possible answers
+var questions = [
+    {
+        q: "What is the first rule of not being seen?",
+        a: "Not standing up",
+        o1: "Better camoflauge",
+        o2: "Standing behind a bush",
+        o3: "Staying deathly quiet"
+    },
+    {
+        q: "Who is the wife of Biggus Dickus?",
+        a: "Incontinentia Buttocks",
+        o1: "Smallus titius",
+        o2: "Larges tookus",
+        o3: "Loudus Mouthus"
+    },
+    {
+        q: "After purchasing a parrot that he believes is dead, the man returns to confront the shopkeep. What does the shopkeeper claim that the Parrot is pining for?",
+        a: "The fjords",
+        o1: "A cracker",
+        o2: "A bit of fresh air",
+        o3: "His homeland"
+    },
+    {
+        q: "According to the Holy Book of Armaments what is the number that though shalt count to before tossing the Holy Hand Grenade of Antioch?",
+        a: "Three",
+        o1: "One",
+        o2: "Two",
+        o3: "Five"
+    },
+    {
+        q: "When King Arthur and his knights are chased by a monster within a cave, how do they escape with their lives?",
+        a: "The animator dies",
+        o1: "They run out the way they came",
+        o2: "They leave behind Sir Gallahad",
+        o3: "They poke at it with the pointy bit of their swords"
+    }];
+
+console.log(questions);
