e4c9567f017d1f1522fdfc01395cfd9917ba26f4
Tutor <tutor.michael78@gmail.com>
2021-07-14 20:13:08 -0500

    added the ability to check for correct answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
-// variable containing the number of questions in the quiz
+// variable containing the number of questions in the quiz and the question number
 var quizLength = 2;
+
+// variable to hold the score
+var score = 0;
 
 // variables that contain the elements of each part of the quiz
 var questionEl = document.querySelector("#question");
@@ -8,6 +11,13 @@ var optionTwoEl = document.querySelector("#option-two");
 var optionThreeEl = document.querySelector("#option-three");
 var optionFourEl = document.querySelector("#option-four");
 
+// variables that allow to check which answer is chosen
+var choiceOneEl = document.querySelector("#choice-one");
+var choiceTwoEl = document.querySelector("#choice-two");
+var choiceThreeEl = document.querySelector("#choice-three");
+var choiceFourEl = document.querySelector("#choice-four");
+
+var submitEl = document.querySelector("#submit");
 
 // array containing all of the questions and possible answers
 var questions = [
@@ -47,48 +57,62 @@ var questions = [
         o3: "They poke at it with the pointy bit of their swords"
     }];
 
-for (var i = 0; i < quizLength; i++) {
-    // variable to pick a random question from the array
-    var questionAsked = questions[Math.floor(Math.random() * questions.length)];
+// variable to pick a random question from the array
+var questionAsked = questions[Math.floor(Math.random() * questions.length)];
 
-    // variables to store random choices from the chosen question
-    var optionChoice1 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
-    var optionChoice2 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
-    var optionChoice3 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
-    var optionChoice4 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
+// variables to store random choices from the chosen question
+var optionChoice1 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
+var optionChoice2 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
+var optionChoice3 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
+var optionChoice4 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
 
-    // adding while loops and if statements to ensure there are no duplicate answers
-    while (optionChoice1 === optionChoice2 || optionChoice1 === optionChoice3 || optionChoice1 === optionChoice4) {
-        if (optionChoice1 === optionChoice2 || optionChoice1 === optionChoice3 || optionChoice1 === optionChoice4) {
-            var optionChoice1 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
-        };
+// adding while loops and if statements to ensure there are no duplicate answers
+while (optionChoice1 === optionChoice2 || optionChoice1 === optionChoice3 || optionChoice1 === optionChoice4) {
+    if (optionChoice1 === optionChoice2 || optionChoice1 === optionChoice3 || optionChoice1 === optionChoice4) {
+        optionChoice1 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
     };
-
-    while (optionChoice2 === optionChoice1 || optionChoice2 === optionChoice3 || optionChoice2 === optionChoice3) {
-        if (optionChoice2 === optionChoice1 || optionChoice2 === optionChoice3 || optionChoice2 === optionChoice3) {
-            optionChoice2 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
-        };
-    };
-
-    while (optionChoice3 === optionChoice1 || optionChoice3 === optionChoice2 || optionChoice3 === optionChoice4) {
-        if (optionChoice3 === optionChoice1 || optionChoice3 === optionChoice2 || optionChoice3 === optionChoice4) {
-            optionChoice3 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
-        };
-    };
-
-    while (optionChoice4 === optionChoice1 || optionChoice4 === optionChoice2 || optionChoice4 === optionChoice3) {
-        if (optionChoice4 === optionChoice1 || optionChoice4 === optionChoice2 || optionChoice4 === optionChoice3) {
-            optionChoice4 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
-        };
-    };
-
-    // setting the question to be displayed
-    questionEl.textContent = questionAsked.q;
-
-    // adding random choices to each option
-    optionOneEl.textContent = optionChoice1;
-    optionTwoEl.textContent = optionChoice2;
-    optionThreeEl.textContent = optionChoice3;
-    optionFourEl.textContent = optionChoice4;
 };
 
+while (optionChoice2 === optionChoice1 || optionChoice2 === optionChoice3 || optionChoice2 === optionChoice3) {
+    if (optionChoice2 === optionChoice1 || optionChoice2 === optionChoice3 || optionChoice2 === optionChoice3) {
+        optionChoice2 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
+    };
+};
+
+while (optionChoice3 === optionChoice1 || optionChoice3 === optionChoice2 || optionChoice3 === optionChoice4) {
+    if (optionChoice3 === optionChoice1 || optionChoice3 === optionChoice2 || optionChoice3 === optionChoice4) {
+        optionChoice3 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
+    };
+};
+
+while (optionChoice4 === optionChoice1 || optionChoice4 === optionChoice2 || optionChoice4 === optionChoice3) {
+    if (optionChoice4 === optionChoice1 || optionChoice4 === optionChoice2 || optionChoice4 === optionChoice3) {
+        optionChoice4 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
+    };
+};
+
+// setting the question to be displayed
+questionEl.textContent = questionAsked.q;
+
+// adding random choices to each option
+optionOneEl.textContent = optionChoice1;
+optionTwoEl.textContent = optionChoice2;
+optionThreeEl.textContent = optionChoice3;
+optionFourEl.textContent = optionChoice4;
+
+
+submitEl.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    if (choiceOneEl.checked === true && optionOneEl.textContent === questionAsked.a) {
+        alert("That is correct");
+    } else if (choiceTwoEl.checked === true && optionTwoEl.textContent === questionAsked.a) {
+        alert("That is correct");
+    } else if (choiceThreeEl.checked === true && optionThreeEl.textContent === questionAsked.a) {
+        alert("That is correct");
+    } else if (choiceFourEl.checked === true && optionFourEl.textContent === questionAsked.a) {
+        alert("That is correct");
+    } else {
+        alert("That is wrong");
+    };
+});
