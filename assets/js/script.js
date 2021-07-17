8d13017b06a17f116195e97f3bfbcce1567a0ec8
Tutor <tutor.michael78@gmail.com>
2021-07-17 14:54:21 -0500

    fixed the problem where questions could repeat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 // variable containing the number of questions in the quiz and the question number
-var quizLength = 2;
+var quizLength = 4;
 var questionNumber = 1;
 
 // variable to hold the score
 var score = 0;
 
 // variables that contain the elements of each part of the quiz
+var titleEl = document.querySelector("#title");
 var questionEl = document.querySelector("#question");
 var optionOneEl = document.querySelector("#option-one");
 var optionTwoEl = document.querySelector("#option-two");
@@ -28,87 +29,121 @@ var questions = [
         a: "Not standing up",
         o1: "Better camoflauge",
         o2: "Standing behind a bush",
-        o3: "Staying deathly quiet"
+        o3: "Staying deathly quiet",
+        title: "Flying Circus",
+        id: 0
     },
     {
         q: "Who is the wife of Biggus Dickus?",
         a: "Incontinentia Buttocks",
         o1: "Smallus titius",
         o2: "Larges tookus",
-        o3: "Loudus Mouthus"
+        o3: "Loudus Mouthus",
+        title: "Life of Brian",
+        id: 1
     },
     {
         q: "After purchasing a parrot that he believes is dead, the man returns to confront the shopkeep. What does the shopkeeper claim that the Parrot is pining for?",
         a: "The fjords",
         o1: "A cracker",
         o2: "A bit of fresh air",
-        o3: "His homeland"
+        o3: "His homeland",
+        title: "Flying Circus",
+        id: 2
     },
     {
         q: "According to the Holy Book of Armaments what is the number that though shalt count to before tossing the Holy Hand Grenade of Antioch?",
         a: "Three",
         o1: "One",
         o2: "Two",
-        o3: "Five"
+        o3: "Five",
+        title: "The Search for the Holy Grail",
+        id: 3
     },
     {
         q: "When King Arthur and his knights are chased by a monster within a cave, how do they escape with their lives?",
         a: "The animator dies",
         o1: "They run out the way they came",
         o2: "They leave behind Sir Gallahad",
-        o3: "They poke at it with the pointy bit of their swords"
+        o3: "They poke at it with the pointy bit of their swords",
+        title: "The Search for the Holy Grail",
+        id: 4
     }];
 
 // variable to pick a random question from the array
 var questionAsked = questions[Math.floor(Math.random() * questions.length)];
 
-// variables to store random choices from the chosen question
-var optionChoice1 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
-var optionChoice2 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
-var optionChoice3 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
-var optionChoice4 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
+// function to choose a question that has not been asked
+var generateQuestion = function() {
+    var existingQuizId = JSON.parse(localStorage.getItem("quizId"));
+    
+    if (existingQuizId == null) {
+        existingQuizId = [];
+        localStorage.setItem("quizId", existingQuizId);
+    }
+
+    for (i = 0; i < existingQuizId.length; i++) {
+        while (questionAsked.id == existingQuizId[i]) {
+            if (questionAsked.id == existingQuizId[i]) {
+                questionAsked = questions[Math.floor(Math.random() * questions.length)];
+            } else {
+                return questionAsked;
+            }
+        };
+    };
+
+    generateOptions(questionAsked);
+    console.log(questionAsked);
+
+    quizId = questionAsked.id
+
+    existingQuizId.push(quizId);
+
+    localStorage.setItem("quizId", JSON.stringify(existingQuizId));
+};
 
 // a function to generate each questions with variety in the options
-var generateQuestion = function () {
+var generateOptions = function (questionOptions) {
+    // variables to store random choices from the chosen question
+    var optionChoice1 = questionOptions[Object.keys(questionOptions)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
+    var optionChoice2 = questionOptions[Object.keys(questionOptions)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
+    var optionChoice3 = questionOptions[Object.keys(questionOptions)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
+    var optionChoice4 = questionOptions[Object.keys(questionOptions)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
 
     // adding while loops and if statements to ensure there are no duplicate answers
     while (optionChoice1 === optionChoice2 || optionChoice1 === optionChoice3 || optionChoice1 === optionChoice4) {
         if (optionChoice1 === optionChoice2 || optionChoice1 === optionChoice3 || optionChoice1 === optionChoice4) {
-            optionChoice1 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
+            optionChoice1 = questionOptions[Object.keys(questionOptions)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
         };
     };
 
     while (optionChoice2 === optionChoice1 || optionChoice2 === optionChoice3 || optionChoice2 === optionChoice3) {
         if (optionChoice2 === optionChoice1 || optionChoice2 === optionChoice3 || optionChoice2 === optionChoice3) {
-            optionChoice2 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
+            optionChoice2 = questionOptions[Object.keys(questionOptions)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
         };
     };
 
     while (optionChoice3 === optionChoice1 || optionChoice3 === optionChoice2 || optionChoice3 === optionChoice4) {
         if (optionChoice3 === optionChoice1 || optionChoice3 === optionChoice2 || optionChoice3 === optionChoice4) {
-            optionChoice3 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
+            optionChoice3 = questionOptions[Object.keys(questionOptions)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
         };
     };
 
     while (optionChoice4 === optionChoice1 || optionChoice4 === optionChoice2 || optionChoice4 === optionChoice3) {
         if (optionChoice4 === optionChoice1 || optionChoice4 === optionChoice2 || optionChoice4 === optionChoice3) {
-            optionChoice4 = questionAsked[Object.keys(questionAsked)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
+            optionChoice4 = questionOptions[Object.keys(questionOptions)[Math.floor(Math.random() * (4 - 1 + 1) + 1)]];
         };
     };
 
-    // setting the question to be displayed
-    questionEl.textContent = questionAsked.q;
+    // setting the question to be displayed and the corresponding title
+    titleEl.textContent = questionOptions.title;
+    questionEl.textContent = questionOptions.q;
 
     // adding random choices to each option
     optionOneEl.textContent = optionChoice1;
     optionTwoEl.textContent = optionChoice2;
     optionThreeEl.textContent = optionChoice3;
     optionFourEl.textContent = optionChoice4;
-};
-
-// function to fetch the question number from local storage 
-var getQuestionNumber = function() {
-    questionNumber = localStorage.getItem("questionNumber");
 };
 
 // function to set the question number to keep track of which question it is
@@ -118,20 +153,21 @@ var setQuestionNumber = function() {
     } else {
         localStorage.setItem("questionNumber", questionNumber);
     }
-}
+};
 
 // function to reset the quiz numbers
 var resetQuiz = function() {
-    getQuestionNumber();
     questionNumber = 0;
     setQuestionNumber();
+    existingQuizId = [];
+    localStorage.setItem("quizId", JSON.stringify(existingQuizId));
     score = 0;
 }
 
 // main function that runs the quiz
 var main = function() {
     // gets the question number from local storage
-    getQuestionNumber();
+    questionNumber = localStorage.getItem("questionNumber");
 
     // checks to see if the questionNumber variable is less than the quiz length
     if (questionNumber < quizLength) {
@@ -154,7 +190,7 @@ var main = function() {
             };
 
             // increments and sets the questionNumber variable to localStorage
-            getQuestionNumber();
+            questionNumber = localStorage.getItem("questionNumber");
             questionNumber++;
             setQuestionNumber();
         });
@@ -170,5 +206,5 @@ var main = function() {
     };
 }
 
-main();
 
+main();
